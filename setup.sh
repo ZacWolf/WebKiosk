d6ccd87c64d4139e3a1008094944c037c09d9256
Zac Morris <zac@zacwolf.com>
2018-02-08 12:22:32 -0500

    readme

--- setup.sh
+++ setup.sh
@@ -4,17 +4,17 @@
 echo Set a new ROOT password:
 passwd
 
-echo "set paste\nset mouse=r" > .vimrc
+echo -e "set paste\nset mouse=r" > .vimrc
 
 apt-get install dbus d-feet less curl bzip2 lsb-compat lsb-release fortune fortunes -y
 
 wget -O -q .bashrc https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/.bashrc
 
-wget -O -q touchscreen.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/touchscreen.sh && chmod 700 .touchscreen.sh && .touchscreen.sh
+wget -O -q touchscreen.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/touchscreen.sh && chmod 700 ./touchscreen.sh && ./touchscreen.sh
 
-wget -O -q setupkiosk.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/setupkiosk.sh && chmod 700 .setupkiosk.sh
+wget -O -q setupkiosk.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/setupkiosk.sh && chmod 700 ./setupkiosk.sh
 
-wget -O -q ir.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/ir.sh && chmod 700 .\ir.sh
+wget -O -q ir.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/ir.sh && chmod 700 ./ir.sh
 
 read -p "The system will shutdown after you press [ENTER].\nIf you chose a touchscreen monitor, unplug the regular monitor, and plugin the touchscreen monitor after the system shuts down and you've removed power."
 
--- touchscreen.sh
+++ touchscreen.sh
@@ -38,11 +38,11 @@ sed -i "s/^setenv\ m_bpp\ /#\ setenv\ m_bpp\ /g" /boot/boot.ini
 sed -i 's/^setenv\ vout\ /#\ setenv\ vout\ /g' /boot/boot.ini
 
 CC=$(whiptail --backtitle "Touchscreen" --menu "Monitor Menu" 0 0 1 --nocancel -ok-button "Select one..."\
-                "1" "Setup VU5/7" \
-                "2" "Setup VU7+" \
-                "3" "Setup VU8" \
-				"4" "Setup for non-touch 1080p monitor" \
-                3>&1 1>&2 2>&3)
+"1" "Setup VU5-7" \
+"2" "Setup VU7plus" \
+"3" "Setup VU8" \
+"4" "Setup non-touch 1080p monitor" \
+3>&1 1>&2 2>&3)
 	case "$CC" in
 			"1")    setup_vu5;;
 			"2")    setup_vu7;;
