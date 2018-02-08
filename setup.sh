72294347e1c76bc9f550ce307103da3622d84726
Zac Morris <zac@zacwolf.com>
2018-02-08 11:39:45 -0500

    readme

--- setup.sh
+++ setup.sh
@@ -16,6 +16,9 @@ chmod 700 .\touchscreen.sh && .\touchscreen.sh
 wget -O setupkiosk.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/setupkiosk.sh
 chmod 700 .\setupkiosk.sh
 
+wget -O ir.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/ir.sh
+chmod 700 .\ir.sh
+
 read -p "The system will shutdown after you press [ENTER].\nIf you chose a touchscreen monitor, unplug the regular monitor, and plugin the touchscreen monitor after the system shuts down and you've removed power."
 
 shutdown -h now
