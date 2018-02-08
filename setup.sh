e072447527aa23f6569b1422540315b7d1a6e323
Zac Morris <zac@zacwolf.com>
2018-02-08 12:13:32 -0500

    readme

--- setup.sh
+++ setup.sh
@@ -8,13 +8,16 @@ echo "set paste\nset mouse=r" > .vimrc
 
 apt-get install dbus d-feet less curl bzip2 lsb-compat lsb-release fortune fortunes -y
 
-wget -O .bashrc https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/.bashrc
+wget -O -q .bashrc https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/.bashrc
 
-wget -O touchscreen.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/touchscreen.sh && chmod 700 .\touchscreen.sh && .\touchscreen.sh
+wget -O -q touchscreen.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/touchscreen.sh
+chmod 700 ./touchscreen.sh && ./touchscreen.sh
 
-wget -O setupkiosk.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/setupkiosk.sh && chmod 700 .\setupkiosk.sh
+wget -O -q setupkiosk.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/setupkiosk.sh
+chmod 700 ./setupkiosk.sh
 
-wget -O ir.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/ir.sh && chmod 700 .\ir.sh
+wget -O -q ir.sh https://raw.githubusercontent.com/ZacWolf/WebKiosk/master/ir.sh
+chmod 700 ./ir.sh
 
 read -p "The system will shutdown after you press [ENTER].\nIf you chose a touchscreen monitor, unplug the regular monitor, and plugin the touchscreen monitor after the system shuts down and you've removed power."
 
